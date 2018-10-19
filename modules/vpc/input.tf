e7031d65c4244027e370c4497b227572f9fbd241
Muhammad Soliman <msuliman@mum.edu>
2018-10-19 10:09:43 -0400

    vpc default names

--- modules/vpc/input.tf
+++ modules/vpc/input.tf
@@ -1,7 +1,7 @@
 variable "vpc" {
   type = "map"
   default = {
-    name = "vpc"
+    name = "my vpc"
     cidr = "10.10.0.0/16"
   }
 }
@@ -10,13 +10,13 @@ variable "subnets" {
   type = "list"
   default = [
     {
-      name = "public"
+      name = "my public subnet"
       type = "public"
       cidr = "10.10.1.0/24"
       az = "us-east-1a"
     },
     {
-      name = "private"
+      name = "my private subnet"
       type = "private"
       cidr = "10.10.2.0/24"
     }
